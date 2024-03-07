8ecb9d2e82ad49843f954832225c03a474704a3b
felichio <fsafaridis@hotmail.com>
2024-03-08 00:25:31 +0200

    add event structure

---
+++ src/event/event.py
@@ -0,0 +1,26 @@
+from enum import Enum
+
+class EventType(Enum):
+    ITEM_RCV = 1
+    EOS = 2
+
+
+class Event:
+
+    def __init__(self, event_type, payload = None):
+        self.event_type = event_type
+        self.payload = payload
+    
+    def event_type(self):
+        return self.event_type
+    
+    def payload(self):
+        return payload
+    
+    @staticmethod
+    def create_itemrcv(index, value):
+        return Event(EventType.ITEM_RCV, {"index": index, "value": value})
+        
+    @staticmethod
+    def create_eos():
+        return Event(EventType.EOS)
--- src/main.py
+++ src/main.py
@@ -1,10 +1,21 @@
-import input.stream_data
+from input.stream_data import StreamData
+from event.event import Event
 
 data = [(1, 2), (3, 4), (5, 6), (7, 9)]
 
 
 def main():
-    sd = input.stream_data.StreamData(data)
+    sd = StreamData(data)
+    queue = []
+    
+    for i, item in enumerate(sd.get_stream_iterator()):
+        ev = Event.create_itemrcv(i + 1, item)
+        queue.append(ev)
+
+    for ev in queue:
+        print(ev.payload)
+    
+
 
 
 if __name__ == "__main__":
