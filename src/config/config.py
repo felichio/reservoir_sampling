9aa999e2fb73ac68648f34ad29de343694938caf
felichio <fsafaridis@hotmail.com>
2024-03-12 15:44:48 +0200

    add support for output

--- src/config/config.py
+++ src/config/config.py
@@ -1,11 +1,21 @@
 import json
 import os
+import copy
 
 CONFIG_FILE = "config.json"
 CONFIG_PATH = os.path.join(os.path.dirname(__file__), CONFIG_FILE)
 
 with open(CONFIG_PATH) as f:
     settings = json.load(f)
+    settings_copy = copy.deepcopy(settings)
     # modify input path
     settings["input"] = os.path.join(os.path.dirname(__file__), "..", "input", settings["input"])
 
+
+def get_output_folder():
+    no = settings_copy["output_directory_number"]
+    settings_copy["output_directory_number"] += 1
+    with open(CONFIG_PATH, "w", encoding = "utf-8") as f:
+        json.dump(settings_copy, f, indent = 4)
+    return no
+    
--- src/consumer/era_handler.py
+++ src/consumer/era_handler.py
@@ -1,5 +1,8 @@
 from event.event import EventType
+from config.config import get_output_folder
+import os
 import copy
+import json
 
 class EraHandler:
 
@@ -14,14 +17,55 @@ class EraHandler:
             self.reservoir_mean_snapshots = []
             self.stream_variance_snapshots = []
             self.reservoir_variance_snapshots = []
+        
+        def fill_dict(self, output, field_name, values):
+            temp = {}
+            for i, item in enumerate(values):
+                temp[i] = item
+            output[field_name] = temp
 
+        def to_json(self):
+            output = {}
+            # stream_data = {}
+            # for i, item in enumerate(self.stream_data):
+            #     stream_data[i] = item
+            # output["stream_data"] = stream_data
+            # reservoir = {}
+            # for i, item in enumerate(self.reservoir_snapshots):
+            #     reservoir[i] = item
+            # output["reservoir"] = reservoir
+            self.fill_dict(output, "stream_data", self.stream_data)
+            self.fill_dict(output, "reservoir", self.reservoir_snapshots)
+            self.fill_dict(output, "stream_mean", self.stream_mean_snapshots)
+            self.fill_dict(output, "reservoir_mean", self.reservoir_mean_snapshots)
+            self.fill_dict(output, "stream_variance", self.stream_variance_snapshots)
+            self.fill_dict(output, "reservoir_variance", self.reservoir_variance_snapshots)
+            
+            return output
 
     def __init__(self, stream_buffer, reservoir_buffer):
         self.stream_buffer = stream_buffer
         self.reservoir_buffer = reservoir_buffer
         self.eras = []
+        
         self.eras.append(EraHandler.Era())
 
+
+    def write_to_output(self):
+        output_path = os.path.join(os.path.dirname(__file__), "..", "output")
+        if not os.path.exists(output_path):
+            os.makedirs(output_path)
+
+        output_path = os.path.join(output_path, f"{str(get_output_folder())}")
+        if not os.path.exists(output_path):
+            os.makedirs(output_path)
+        
+        output = {}
+        for i, item in enumerate(self.eras):
+            output[f"era_{i + 1}"] = item.to_json()
+        
+        with open(os.path.join(output_path, "eras.json"), "w", encoding = "utf-8") as f:
+            json.dump(output, f, indent = 2)
     
     def is_era_completed(self):
         pass
@@ -55,3 +99,6 @@ class EraHandler:
         if event.event_type == EventType.EOS:
             print(f"EraHandler consuming {event.event_type} with payload {event.payload}")
             self.complete_era()
+            self.write_to_output()
+    
+
