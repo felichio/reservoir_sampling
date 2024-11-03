9a7efe770f5ee33e6efec5c8d800f7b4d307427c
felichio <fsafaridis@hotmail.com>
2024-11-03 23:30:46 +0200

    add arguments to plotter

--- src/plotter.py
+++ src/plotter.py
@@ -8,13 +8,33 @@ from plotconverter.plot_converter import PlotType
 from plotconverter.mean_plotter import MeanPlotter
 from plotconverter.variance_plotter import VariancePlotter
 from plotconverter.coefficientvar_plotter import CoefficientVarPlotter
+import argparse
 
 
 def main():
     
+    parser = argparse.ArgumentParser(prog="Plotter for Reservoir Sampling Simulation")
+    parser.add_argument("-e", "--eras", default=-1, type=int, nargs="*" )
+    parser.add_argument("-s", "--simulation", default=-1, type=int)
+    parser.add_argument("-d", "--dimension", default=1)
+    parser.add_argument("-r", "--range", default=[], nargs=2, type=int)
+    parser.add_argument("-p", "--plot", choices=["mean", "variance", "coefficient_variance"], default="coefficient_variance")
+
+    args = parser.parse_args()
+    if args.eras == -1:
+        args.eras = "all"
+    if args.simulation == -1:
+        args.simulation = "last"
     
-    mp = CoefficientVarPlotter(era_n = "all", simulation_n = "last", dimension_n = 1)
-    mp.plot()
+    
+
+    if args.plot == "mean":
+        p = MeanPlotter(era_n=args.eras, simulation_n=args.simulation, dimension_n=args.dimension)
+    elif args.plot == "variance":
+        p = VariancePlotter(era_n=args.eras, simulation_n=args.simulation, dimension_n=args.dimension)
+    elif args.plot == "coefficient_variance":
+        p = CoefficientVarPlotter(era_n=args.eras, simulation_n=args.simulation, dimension_n=args.dimension)
+    p.plot(args.range)
 
 
     
